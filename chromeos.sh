654a594b4aecf3f9124119938be9ceff0b1004ac
H-Plus-Time <nicholas.roberts.au@gmail.com>
2016-08-22 15:12:11 +1000

    Minor tweaks, added py27env (required for GAE SDK)

--- chromeos.sh
+++ chromeos.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y vim git curl clang unzip wget entr
 
 ## Install rvm, rake, ruby, gem
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash
+\curl -sSL https://get.rvm.io | bash -s stable --autolibs=enabled --ruby
 source ~/.zshrc
 source $HOME/.rvm/scripts/rvm
 
