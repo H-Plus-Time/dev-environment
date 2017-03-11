dcc44621dc06cccd434c80402043ba8450364eb0
+Nicholas Roberts <nicholas.roberts.au@gmail.com>
2017-03-11 17:05:58 +1100

    Fixed partial.zshrc copy

--- shared-nix/preinstall.sh
+++ shared-nix/preinstall.sh
@@ -1,6 +1,6 @@
 curl -sL zplug.sh/installer | zsh
 echo "source ~/.zplug/init.zsh" >> $HOME/.zshrc
-cat partial.zshrc >> $HOME/.zshrc
+cat shared-nix/partial.zshrc >> $HOME/.zshrc
 
 echo "zplug check || zplug install" >> $HOME/.zshrc
 echo "zplug load" >> $HOME/.zshrc
